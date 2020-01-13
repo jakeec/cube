d5cf8a04924792dee5d891ea4e15fcf852605afa
Jake Carrington <jake.ethan.carrington@gmail.com>
2020-01-13 20:55:04 +0000

    Can rotate the top face

--- src/main.rs
+++ src/main.rs
@@ -35,8 +35,8 @@ impl Cube {
     fn new() -> Self {
         Cube {
             Up: Face(vec![
-                vec![Color::Red; 3],
-                vec![Color::White; 3],
+                vec![Color::Black; 3],
+                vec![Color::Blue; 3],
                 vec![Color::Green; 3],
             ]),
             Down: Face::new(Color::Yellow),
@@ -95,37 +95,95 @@ impl Cube {
 
     fn draw_square(&self, color: Color) {
         match color {
-            Color::White => print!("\u{2589} "),
-            Color::Yellow => print!("\x1b[0;35m\u{2589}\x1b[0m "),
-            Color::Orange => print!("\x1b[0;33m\u{2589}\x1b[0m "),
-            Color::Red => print!("\x1b[38;5;160m\u{2589}\x1b[0m "),
-            Color::Green => print!("\x1b[0;32m\u{2589}\x1b[0m "),
-            Color::Blue => print!("\x1b[34;5m\u{2589}\x1b[0m "),
-            Color::Black => print!("\x1b[0;30m\u{2589}\x1b[0m "),
+            Color::White => print!("\u{25A0} "),
+            Color::Yellow => print!("\x1b[0;35m\u{25A0}\x1b[0m "),
+            Color::Orange => print!("\x1b[0;33m\u{25A0}\x1b[0m "),
+            Color::Red => print!("\x1b[38;5;160m\u{25A0}\x1b[0m "),
+            Color::Green => print!("\x1b[0;32m\u{25A0}\x1b[0m "),
+            Color::Blue => print!("\x1b[34;5m\u{25A0}\x1b[0m "),
+            Color::Black => print!("\x1b[0;30m\u{25A0}\x1b[0m "),
         }
     }
 
-    fn up_clockwise(&mut self) {
-        // rotate Up face 90deg
-        let face = match &self.Up {
+    fn face_cw(face: &Face) -> Face {
+        let face = match face {
             Face(face) => face,
         };
         let mut new = face.clone();
         for i in 0..face.len() {
             for j in 0..face[i].len() {
-                new[i][j] = face[j][i];
+                new[j][face.len() - 1 - i] = face[i][j];
             }
         }
-        self.Up = Face(new);
+        Face(new)
+    }
 
-        // shift top slice of each horizontal face across three spaces
+    fn face_ccw(face: &Face) -> Face {
+        let face = match face {
+            Face(face) => face,
+        };
+        let mut new = face.clone();
+        for i in 0..face.len() {
+            for j in 0..face[i].len() {
+                new[i][j] = face[j][face[i].len() - 1 - i];
+            }
+        }
+        Face(new)
+    }
+
+    fn rotate_slice(
+        faces: (&Face, &Face, &Face, &Face),
+        row: usize,
+    ) -> (Vec<Color>, Vec<Color>, Vec<Color>, Vec<Color>) {
+        let (first, second, third, fourth) = match (faces.0, faces.1, faces.2, faces.3) {
+            (Face(first), Face(second), Face(third), Face(fourth)) => (
+                first.to_owned(),
+                second.to_owned(),
+                third.to_owned(),
+                fourth.to_owned(),
+            ),
+        };
+
+        (
+            first[row].to_owned(),
+            second[row].to_owned(),
+            third[row].to_owned(),
+            fourth[row].to_owned(),
+        )
+    }
+
+    fn up_cw(&mut self) {
+        self.Up = Self::face_cw(&self.Up);
+
+        let (left, front, right, back) =
+            Self::rotate_slice((&self.Left, &self.Front, &self.Right, &self.Back), 0);
+
+        self.Back.0[0] = left;
+        self.Right.0[0] = back;
+        self.Front.0[0] = right;
+        self.Left.0[0] = front;
+    }
+
+    fn up_ccw(&mut self) {
+        self.Up = Self::face_ccw(&self.Up);
+
+        let (left, front, right, back) =
+            Self::rotate_slice((&self.Left, &self.Front, &self.Right, &self.Back), 0);
+
+        self.Back.0[0] = right;
+        self.Right.0[0] = front;
+        self.Front.0[0] = left;
+        self.Left.0[0] = back;
     }
 }
 
 fn main() {
     let mut cube = Cube::new();
     cube.print();
-    cube.up_clockwise();
+    cube.up_cw();
+    println!("");
+    cube.print();
+    cube.up_ccw();
     println!("");
     cube.print();
 }
