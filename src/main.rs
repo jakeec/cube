9960822b750e6c8f8801262aed267a1d6bf8c8b9
Jake Carrington <jake.ethan.carrington@gmail.com>
2020-01-15 21:33:06 +0000

    Added a method for instant feedback input

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 mod cube;
 use cube::Cube;
-use std::io::{stdin, Read, Stdout, Write};
+use getch::Getch;
+use std::io;
+use std::io::{stdin, Read, Stdin, StdinLock, Stdout, Write};
+use std::str;
 
 fn main() {
     println!("CUBE");
     let mut cube = Cube::new();
+    let getch = Getch::new();
     loop {
-        let mut input = String::from("");
-        println!("Enter your moves: ");
-        stdin().read_line(&mut input).unwrap();
-        cube.input(input.trim());
+        let input = getch.getch().unwrap();
+        cube.input(str::from_utf8(&[input]).unwrap());
         cube.print();
     }
+    // loop {
+    //     let mut input = String::from("");
+    //     println!("Enter your moves: ");
+    //     stdin().read_line(&mut input).unwrap();
+    //     cube.input(input.trim());
+    //     cube.print();
+    // }
 }
